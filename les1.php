e0480dacbf2f15514e85e5c76b43df659f30fc78
Nick <32934@ma-web.nl>
2023-09-07 12:34:43 +0200

    web wordt nu ook dikgedrukt als het een even getal is

--- les1.php
+++ les1.php
@@ -2,16 +2,18 @@
 
 for ($x = 33; $x <= 543; $x++) { // for loop van getallen 33 t/m 543
     if ($x % 5 == 0 && $x % 9 == 0) { // als het getal deel baar is door 5 en 9 dan is "web" zichtbaar op het scherm
-        echo "web <br>";
-    } 
-    elseif ($x % 5 == 0) { // als het getal deerbaar is door 5 toont "software" op het scherm
+        if ($x % 2 == 0) { // Als het getal een even nummer is dan wordt het dikgedrukt 
+            echo "<b>web </b><br>";
+
+        } else // anders krijg je "web" niet dikgedrukt
+            echo "web <br>";
+    } elseif ($x % 5 == 0) { // als het getal deerbaar is door 5 toont "software" op het scherm
         if ($x % 2 == 0) { // Als het getal een even nummer is dan wordt het dikgedrukt 
             echo "<b>software </b><br>";
 
         } else // anders krijg je software niet dikgedrukt
             echo "software <br>";
-    } 
-    elseif ($x % 9 == 0) { // Als het getal deelbaar is door 9 toont "developer" op het scherm
+    } elseif ($x % 9 == 0) { // Als het getal deelbaar is door 9 toont "developer" op het scherm
         if ($x % 2 == 0) { // Als het getal een even nummer is dan wordt het dikgedrukt
             echo "<b>developer </b><br>";
 
