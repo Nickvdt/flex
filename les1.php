f1c6ef3b76fc429a59f8050565fd482fc58e1a84
Nick <32934@ma-web.nl>
2023-09-07 12:16:23 +0200

    Even getallen vetgedrukt

--- les1.php
+++ les1.php
@@ -1,7 +1,15 @@
 <?php
 
 for ($x = 33; $x <= 543; $x++) {
-    echo "$x <br>";
+
+
+    if ($x % 2 == 0) {
+        echo "<b> $x </b> <br>";
+    } 
+    else {
+        echo "$x <br>";
+    }
 }
+
 
 ?>
