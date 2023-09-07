19e97144b7b8166f5cf8679d7621712844fc0916
Nick <32934@ma-web.nl>
2023-09-07 12:33:30 +0200

    Comments toegevoegd

--- les1.php
+++ les1.php
@@ -1,25 +1,26 @@
 <?php
 
-for ($x = 33; $x <= 543; $x++) {
-    if ($x % 5 == 0 && $x % 9 == 0) {
+for ($x = 33; $x <= 543; $x++) { // for loop van getallen 33 t/m 543
+    if ($x % 5 == 0 && $x % 9 == 0) { // als het getal deel baar is door 5 en 9 dan is "web" zichtbaar op het scherm
         echo "web <br>";
-    } elseif ($x % 5 == 0) {
-        if ($x % 2 == 0){
+    } 
+    elseif ($x % 5 == 0) { // als het getal deerbaar is door 5 toont "software" op het scherm
+        if ($x % 2 == 0) { // Als het getal een even nummer is dan wordt het dikgedrukt 
             echo "<b>software </b><br>";
-            
-        }else echo "software <br>";
-    }
 
-
-     elseif ($x % 9 == 0) {
-        if ($x % 2 == 0){
+        } else // anders krijg je software niet dikgedrukt
+            echo "software <br>";
+    } 
+    elseif ($x % 9 == 0) { // Als het getal deelbaar is door 9 toont "developer" op het scherm
+        if ($x % 2 == 0) { // Als het getal een even nummer is dan wordt het dikgedrukt
             echo "<b>developer </b><br>";
-            
-        }else echo "developer <br>";
-        
-    } elseif ($x % 2 == 0) {
+
+        } else // anders krijg je developer niet dikgedrukt
+            echo "developer <br>";
+
+    } elseif ($x % 2 == 0) { // even getallen worden hier dikgedrukt
         echo "<b> $x </b> <br>";
-    } else {
+    } else { // anders wordt het niet dikgedrukt
         echo "$x <br>";
     }
 }
