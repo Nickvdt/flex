903c1f7400d1d326969f01502748b00970e26588
Nick <32934@ma-web.nl>
2023-09-07 12:24:19 +0200

    Software en Developer tonen op het scherm

--- les1.php
+++ les1.php
@@ -1,14 +1,22 @@
 <?php
 
 for ($x = 33; $x <= 543; $x++) {
-
-
-    if ($x % 2 == 0) {
+    if ($x % 5 == 0 && $x % 9 == 0){
+        echo "web <br>";
+    }
+    elseif ($x % 5 == 0) {
+        echo "software <br>";
+    }
+    elseif ($x % 9 == 0) {
+        echo "developer <br>";
+    }
+    elseif ($x % 2 == 0) {
         echo "<b> $x </b> <br>";
     } 
     else {
         echo "$x <br>";
     }
+
 }
 
 
